fce16925612a1dfff3b8ebd0ec16773fd7ba9eb6
nsingrid <ingrid.nascimento1@outlook.com>
2019-04-12 21:46:46 -0300

    IMC empoderado

---
+++ palindromo/script.js
@@ -0,0 +1,15 @@
+function palindromo() {
+    var palavra = document.getElementById("input").value.toLowerCase();
+    var resposta = document.getElementById("resposta");
+
+    for(var i = 0; i < palavra.length/2; i++) {
+        if (palavra[i] !== palavra[palavra.length -i -1]) {
+            resposta.innerText = "Não é 😟"
+            return
+        }
+        else {
+            resposta.innerText = "Simm! 😄"
+        }
+    }
+
+}
